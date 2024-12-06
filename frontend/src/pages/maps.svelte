caed197cdf6bc04f7c06175d9a9217e6586f0eb8
xin <xinyihsu1458@gmail.com>
2024-12-06 16:40:15 +0800

    chore(frontend): add locate user

--- frontend/src/pages/maps.svelte
+++ frontend/src/pages/maps.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { Page } from "framework7-svelte";
-  import { tick } from "svelte";
+  import { Button, Icon, Page } from "framework7-svelte";
+  import { onDestroy, onMount, tick } from "svelte";
   import { MapLibre, Marker } from "svelte-maplibre";
   import type { LngLatLike } from "svelte-maplibre/dist/types";
   import PinDetailsSheet from "../components/PinDetailsSheet.svelte";
@@ -26,6 +26,7 @@
   let sheetClosing = false;
 
   let focusPin: Pin | null = null;
+  let userPin: LngLatLike = null;
 
   let sheetView: HTMLElement | null = null;
 
@@ -68,6 +69,53 @@
     focusPin = null;
     sheetClosing = false;
   }
+
+  const locateUser = () => {
+    if (!navigator.geolocation) {
+      console.error("您的设备不支持地理定位功能。");
+      return;
+    }
+
+    if (userPin == null) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          userPin = [longitude, latitude];
+        },
+        (error) => {
+          console.error("定位失败：", error);
+        }
+      );
+    }
+
+    map.flyTo({
+      center: userPin,
+      zoom,
+      speed: 1.2,
+    });
+  };
+
+  onMount(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(updateUserPosition, () => {}, {
+        enableHighAccuracy: true,
+      });
+    } else {
+      alert("瀏覽器不支援地理定位功能。");
+    }
+  });
+
+  // 更新用戶位置
+  function updateUserPosition(position) {
+    const { latitude, longitude } = position.coords;
+    userPin = [longitude, latitude];
+  }
+
+  onDestroy(() => {
+    if (map) {
+      map.remove();
+    }
+  });
 </script>
 
 <Page noNavbar={true}>
@@ -92,6 +140,15 @@
         {/if}
       </Marker>
     {/each}
+    <!-- user location -->
+    <Marker lngLat={userPin} class="relative">
+      <div
+        class="h-4 w-4 bg-[#7D7DF1] rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      ></div>
+      <div
+        class="h-20 w-20 bg-[#3B3BD026] rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      ></div>
+    </Marker>
   </MapLibre>
 
   <PinDetailsSheet
@@ -101,6 +158,31 @@
     onSheetClose={handleSheetClose}
     onSheetClosed={handleSheetClosed}
   />
+  <div
+    class="flex flex-row justify-center w-full pb-24 fixed bottom-0 z-5 pointer-events-none"
+  >
+    <div class="w-9/12 flex flex-row justify-between">
+      <Button
+        outline
+        class="w-[62px] h-[62px] border-[1px] border-black bg-white rounded-[10px] pointer-events-auto drop-shadow-[0_4px_6px_rgba(0,0,0,0.25)]"
+      >
+        <Icon material="search" size="28px" class="color-black"></Icon>
+      </Button>
+      <Button
+        on:click={locateUser}
+        outline
+        class="w-[62px] h-[62px] border-[1px] border-black bg-white rounded-[10px] pointer-events-auto drop-shadow-[0_4px_6px_rgba(0,0,0,0.25)]"
+      >
+        <Icon material="my_location" size="28px" class="color-black"></Icon>
+      </Button>
+      <Button
+        outline
+        class="w-[62px] h-[62px] border-[1px] border-black bg-white rounded-[10px] pointer-events-auto drop-shadow-[0_4px_6px_rgba(0,0,0,0.25)]"
+      >
+        <Icon material="account_circle" size="28px" class="color-black"></Icon>
+      </Button>
+    </div>
+  </div>
 </Page>
 
 <style lang="postcss">
